f260e7f8df233e2d32929dc451c0851b3f4ca717
thangckt <thangckt@gmail.com>
2025-05-12 09:07:53 +0000

    Update from https://github.com/thangckt/thutil/commit/8c4ed5923abc62d542c57c22e099e08a1e6b6f04

---
+++ _script/py_copy_config_schema.py
@@ -0,0 +1,41 @@
+def append_schema_text(yaml_schema_file: str, text_file: str):
+    """Append yaml text to .md file."""
+    with open(yaml_schema_file, "r") as f:
+        text = f.read()
+    schema_text = "\n```yaml\n" + text + "\n```\n"
+    schema_text = "\n## Schema:\n" + schema_text
+    ### append yaml schema
+    with open(text_file, "a") as f:
+        f.write(schema_text)
+    return
+
+
+def append_example_config(yaml_config_file: str, text_file: str):
+    """Append yaml text to .md file."""
+    with open(yaml_config_file, "r") as f:
+        text = f.read()
+    schema_text = "\n```yaml\n" + text + "\n```\n"
+    schema_text = "\n## Example configuration:\n" + schema_text
+    ### append yaml schema
+    with open(text_file, "a") as f:
+        f.write(schema_text)
+    return
+
+
+##### ANCHOR: udpate the blog posts
+def main():
+    ### Append schema to the .md files
+    append_schema_text(
+        yaml_schema_file="./thutil/schema/schema_arg_remote_machine.yml",
+        text_file="./_docs/schema/config_remote_machine.md",
+    )
+    ### Append example configuration to the .md files
+    # append_example_config(
+    #     yaml_config_file="./thutil/schema/example_param_active_learn.yml",
+    #     text_file="./_docs/schema/config_active_learning.md",
+    # )
+    return
+
+
+if __name__ == "__main__":
+    main()
